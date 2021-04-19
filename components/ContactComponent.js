37b7d1208672f8a4c8b0982ffeea4cfcb0114f66
Sebastien Laifa <seblaifa@hotmail.com>
2021-04-19 10:11:54 -0700

    3. Exercise: Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,29 +1,35 @@
-import React, { Component } from 'react';
-import { Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements'
+import React, { Component } from "react";
+import { Text, ScrollView } from "react-native";
+import { Card } from "react-native-elements";
+import * as Animatable from "react-native-animatable";
 
 class Contact extends Component {
-
     static navigationOptions = {
-        title: 'Contact Us'
+        title: "Contact Us",
     };
 
     render() {
         return (
-            <ScrollView style={{backgroundColor:"whitesmoke"}}>
-                <Card
-                    title="Contact Information"
-                    wrapperStyle={{margin: 20}}
+            <ScrollView style={{ backgroundColor: "whitesmoke" }}>
+                <Animatable.View
+                    animation="fadeInDown"
+                    duration={2000}
+                    delay={1000}
                 >
-                    <Text>1 Nucamp Way</Text>
-                    <Text>Seattle, WA 98001</Text>
-                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
-                    <Text>Phone: 1-206-555-1234</Text>
-                    <Text>Email: campsites@nucamp.co</Text>
-                </Card>
+                    <Card
+                        title="Contact Information"
+                        wrapperStyle={{ margin: 20 }}
+                    >
+                        <Text>1 Nucamp Way</Text>
+                        <Text>Seattle, WA 98001</Text>
+                        <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+                        <Text>Phone: 1-206-555-1234</Text>
+                        <Text>Email: campsites@nucamp.co</Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
-        )
+        );
     }
 }
 
-export default Contact
+export default Contact;
--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,41 +1,44 @@
-import React, { Component } from 'react';
-import { View, FlatList, Text } from 'react-native';
-import { Tile } from 'react-native-elements';
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
-import Loading from './LoadingComponent';
+import React, { Component } from "react";
+import { View, FlatList, Text } from "react-native";
+import { Tile } from "react-native-elements";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
+import * as Animatable from "react-native-animatable";
 
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
-        campsites: state.campsites
+        campsites: state.campsites,
     };
 };
 
 class Directory extends Component {
-
     static navigationOptions = {
-        title: 'Directory'
+        title: "Directory",
     };
 
     render() {
         const { navigate } = this.props.navigation;
-        const renderDirectoryItem = ({item}) => {
+        const renderDirectoryItem = ({ item }) => {
             return (
-                <Tile
-                    title={item.name}
-                    caption={item.description}
-                    featured
-                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-                    imageSrc={{uri: baseUrl + item.image}}
-                />
+                <Animatable.View animation="fadeInRightBig" duration={2000}>
+                    <Tile
+                        title={item.name}
+                        caption={item.description}
+                        featured
+                        onPress={() =>
+                            navigate("CampsiteInfo", { campsiteId: item.id })
+                        }
+                        imageSrc={{ uri: baseUrl + item.image }}
+                    />
+                </Animatable.View>
             );
         };
 
         if (this.props.campsites.isLoading) {
             return <Loading />;
         }
-        
+
         if (this.props.campsites.errMess) {
             return (
                 <View>
@@ -48,7 +51,7 @@ class Directory extends Component {
             <FlatList
                 data={this.props.campsites.campsites}
                 renderItem={renderDirectoryItem}
-                keyExtractor={item => item.id.toString()}
+                keyExtractor={(item) => item.id.toString()}
             />
         );
     }
