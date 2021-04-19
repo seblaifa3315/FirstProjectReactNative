4037029233a725b64194f86b9e70396e4111c052
Sebastien Laifa <seblaifa@hotmail.com>
2021-04-19 13:42:18 -0700

    2. Exercise: Persist Redux Store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,23 +1,33 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { campsites } from './campsites';
-import { comments } from './comments';
-import { promotions } from './promotions';
-import { partners } from './partners';
-import { favorites } from './favorites';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { campsites } from "./campsites";
+import { comments } from "./comments";
+import { promotions } from "./promotions";
+import { partners } from "./partners";
+import { favorites } from "./favorites";
+import { persistStore, persistCombineReducers } from "redux-persist";
+import storage from "redux-persist/es/storage";
+
+const config = {
+    key: "root",
+    storage,
+    debug: true,
+};
 
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             campsites,
             comments,
             partners,
             promotions,
-            favorites
+            favorites,
         }),
         applyMiddleware(thunk, logger)
     );
 
-    return store;
-}
+    const persistor = persistStore(store);
+
+    return { persistor, store };
+};
