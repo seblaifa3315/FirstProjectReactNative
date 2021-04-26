1c532012e71cc953eaad67324409426d18d6537f
Sebastien Laifa <seblaifa@hotmail.com>
2021-04-26 10:52:13 -0700

    3. Exercise: Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -9,6 +9,7 @@ import {
     StyleSheet,
     Alert,
     PanResponder,
+    Share,
 } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
@@ -79,6 +80,19 @@ function RenderCampsite(props) {
         },
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share(
+            {
+                title: title,
+                message: `${title}: ${message} ${url}`,
+                url: url,
+            },
+            {
+                dialogTitle: "Share " + title,
+            }
+        );
+    };
+
     if (campsite) {
         return (
             <Animatable.View
@@ -107,12 +121,26 @@ function RenderCampsite(props) {
                             }
                         />
                         <Icon
-                            name="pencil"
+                            name={"pencil"}
                             type="font-awesome"
                             color="#5637DD"
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name={"share"}
+                            type="font-awesome"
+                            color="#5637DD"
+                            raised
+                            reverse
+                            onPress={() =>
+                                shareCampsite(
+                                    campsite.name,
+                                    campsite.description,
+                                    baseUrl + campsite.image
+                                )
+                            }
                         />
                     </View>
                 </Card>
